f868910f65f4ddc5afdda0e024ca26c76a0dd98f
simon qiang <simonqiang@gmail.com>
2016-02-29 16:22:38 +0000

    test passed

--- functional_tests.py
+++ functional_tests.py
@@ -4,7 +4,7 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(10)
 
     def tearDown(self):
         self.browser.quit()
